a8bf9ca717444496e4f2c78a97476abb830817ff
HamzaPapicc <hamza70.hp@gmail.com>
2024-02-10 16:39:24 +0100

    commit

---
+++ zdravostopalo/src/renderer/src/components/MyCalendar.jsx
@@ -0,0 +1,17 @@
+import { Calendar, dayjsLocalizer } from 'react-big-calendar'
+import dayjs from 'dayjs'
+import "react-big-calendar/lib/css/react-big-calendar.css"
+
+const localizer = dayjsLocalizer(dayjs)
+
+export default function MyCalendar() {
+  <div>
+    <Calendar
+      localizer={localizer}
+      //events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+}
